0d44735d56589bce32d39e3766ffd8c52b1d32fb
carbonroast <zergling3@gmail.com>
2017-09-28 00:52:14 -0400

    multiplayer start

---
+++ Assets/Scripts/CSVParser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class CSVParser : MonoBehaviour {
+	public TextAsset file;
+	public int maxColumnNumber;
+	private List<Dictionary<string, List<string>>> Reciple = new List<Dictionary<string, List<string>>>();
+	//private TextAsset data = Resources.Load (file) as TextAsset;
+	private string pattern = ",|\r";
+	public void Start(){
+		Debug.Log ("Test Start");
+		string[] lineparse = Regex.Split(file.text,pattern);
+		for (int i = 0;	i < lineparse.Length; i=i+4) {
+			Debug.Log (lineparse[i] + " " + lineparse[(i + 1)] + " "  + " " + lineparse[(i + 2)] + " " + lineparse[(i + 3)] + "\n");
+		}
+	}
+	void Awake(){
+		Debug.Log ("Test Start awakeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee hcgfgfghfgh vgfgjghhgjjghgjh hjgk jhjhg ghjg jgh");
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+	//public GameObject player;
+	private Vector3 position;
+	protected NavMeshAgent player;
+	public LayerMask canMove;
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Click ();
+	}
+	void Click(){
+		if (Input.GetMouseButtonDown (0)) {
+			//Debug.Log ("down");
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit, 100f, canMove.value)) {
+				Debug.Log ("position is " + hit.point);
+				position = hit.point;
+				//transform.position = position;
+
+				player.destination = position;
+
+			}
+		}
+
+	}
+
+}
